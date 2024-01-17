c800bdb2347a504cd8e2884d12a1aa90eed80496
盒马盒马 <2353151024@qq.com>
2024-01-17 20:14:25 +0800

    code

---
+++ bit/24.01.17/leetcode.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int maximumNumberOfStringPairs(char** words, int wordsSize) {
+
+}
+
+int main()
+{
+
+	return 0;
+}
